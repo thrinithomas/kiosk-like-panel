7c7fecc187b8ad757353251b5e67dfcae5d812a0
Ka Yuen Leung <thomas060142@icloud.com>
2020-03-11 01:31:18 +0800

    Updates

---
+++ precache-manifest.f909cc3eabe337111964ab2dff03dd05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e0471a20945b98e40d44543a262e2a",
+    "url": "/kiosk-like-panel/index.html"
+  },
+  {
+    "revision": "65c19b3780913292f3f5",
+    "url": "/kiosk-like-panel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1aba2e6dc70e987d812f",
+    "url": "/kiosk-like-panel/static/js/2.ca4adaa2.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/kiosk-like-panel/static/js/2.ca4adaa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c19b3780913292f3f5",
+    "url": "/kiosk-like-panel/static/js/main.27240b44.chunk.js"
+  },
+  {
+    "revision": "dd6ccabd244f9d5806c6",
+    "url": "/kiosk-like-panel/static/js/runtime-main.8ef0e60d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kiosk-like-panel/static/media/logo.5d5d9eef.svg"
+  }
+]);
